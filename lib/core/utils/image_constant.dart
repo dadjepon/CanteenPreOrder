4e45d84be6606edea40405df9be2619ca7d4da4e
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-11-25 13:45:46 +0000

    Update Dashboard UI

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,82 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // SF - Splash screen images
+  static String imgGrillChickenSpicy = '$imagePath/img_grill_chicken_spicy.png';
+
+  static String imgScreenshot2023 = '$imagePath/img_screenshot_2023.png';
+
+  // SF - Dashboard images
+  static String imgPizza52 = '$imagePath/img_pizza_5_2.png';
+
+  static String imgPlus = '$imagePath/img_plus.svg';
+
+  static String imgVectorWhiteA7001x18 =
+      '$imagePath/img_vector_white_a700_1x18.svg';
+
+  // SF - Food item images
+  static String imgMaskGroup = '$imagePath/img_mask_group.png';
+
+  static String imgVectorWhiteA70035x34 =
+      '$imagePath/img_vector_white_a700_35x34.svg';
+
+  static String imgVector = '$imagePath/img_vector.svg';
+
+  static String imgMaskGroup94x94 = '$imagePath/img_mask_group_94x94.png';
+
+  // SF-Cart images
+  static String imgImage2 = '$imagePath/img_image_2.png';
+
+  static String imgClose = '$imagePath/img_close.svg';
+
+  static String imgImage280x91 = '$imagePath/img_image_2_80x91.png';
+
+  // SF - Payment images
+  static String imgCloseOnerrorcontainer =
+      '$imagePath/img_close_onerrorcontainer.svg';
+
+  // SF - payment success images
+  static String imgVectorTeal400 = '$imagePath/img_vector_teal_400.svg';
+
+  // SF - Receipt images
+  static String imgInfo = '$imagePath/img_info.svg';
+
+  static String imgMaximize = '$imagePath/img_maximize.svg';
+
+  static String imgVuesaxOutlineTruckTime =
+      '$imagePath/img_vuesax_outline_truck_time.svg';
+
+  static String imgVuesaxOutlineBoxRemove =
+      '$imagePath/img_vuesax_outline_box_remove.svg';
+
+  // SF - Profile images
+  static String imgRectangle6 = '$imagePath/img_rectangle_6.png';
+
+  static String imgArrowleftWhiteA700 =
+      '$imagePath/img_arrowleft_white_a700.svg';
+
+  // Frame 28972 images
+  static String imgFile = '$imagePath/img_file.svg';
+
+  static String imgImage284x91 = '$imagePath/img_image_2_84x91.png';
+
+  static String imgVisaLogo = '$imagePath/img_visa_logo.svg';
+
+  static String imgArrowLeftOnerrorcontainer =
+      '$imagePath/img_arrow_left_onerrorcontainer.png';
+
+  static String imgVectorOnerrorcontainer =
+      '$imagePath/img_vector_onerrorcontainer.svg';
+
+  // Common images
+  static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+
+  static String imgSearch = '$imagePath/img_search.png';
+
+  static String imgVectorPrimary = '$imagePath/img_vector_primary.svg';
+
+  static String imgVectorWhiteA700 = '$imagePath/img_vector_white_a700.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
